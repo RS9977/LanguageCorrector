4b98660c06112d039208718bb0fb005b879e2d58
Reza99 <sajjadi@bu.edu>
2024-04-26 22:54:11 -0400

    fixColoringOfChecker

--- CheckerCorrector/GUI/HighlighterGUI.java
+++ CheckerCorrector/GUI/HighlighterGUI.java
@@ -5,7 +5,9 @@ import javax.swing.text.DefaultHighlighter;
 
 import DBinterface.DBinterface;
 import DirectedGraph.BasicGraph;
+import HashTableMaker.HashTableMaker;
 
+import java.sql.SQLException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -52,7 +54,7 @@ public class HighlighterGUI extends JFrame {
     private List<String> extractPhrases(String text, int phraseLength) {
         List<String> phrases = new ArrayList<>();
         String[] words = text.split("\\s+");
-        for (int i = 0; i <= words.length - phraseLength; i+=phraseLength) {
+        for (int i = 0; i <= words.length - phraseLength; i+=1) {
             StringBuilder phraseBuilder = new StringBuilder();
             for (int j = 0; j < phraseLength; j++) {
                 phraseBuilder.append(words[i + j]);
@@ -66,21 +68,32 @@ public class HighlighterGUI extends JFrame {
     }
 
     private void highlightPhrases(List<String> phrases) {
-        DBinterface dbInterface;
-        if(!this.isDutch){
-            dbInterface = new DBinterface("SQLite/token_database_english.db", "SQLite/smallDic.txt");
-        }else{
-            dbInterface = new DBinterface("SQLite/token_database_dutch.db", "SQLite/DutchTranslation.txt");
-        }
-        BasicGraph basicGraphClass = new BasicGraph();
-        for (String phrase : phrases) {
-            highlightPhrase(phrase, dbInterface.checkTokenInDatabase(phrase, basicGraphClass.getGraph()));
+        try{
+            DBinterface dbInterface;
+            HashTableMaker manager;
+            if(!this.isDutch){
+                dbInterface = new DBinterface("SQLite/token_database_english.db", "SQLite/smallDic.txt");
+                manager = new HashTableMaker("SQLite/hash_database_english.db");
+            }else{
+                dbInterface = new DBinterface("SQLite/token_database_dutch.db", "SQLite/DutchTranslation.txt");
+                manager = new HashTableMaker("SQLite/hash_database_dutch.db");
+            }
+            BasicGraph basicGraphClass = new BasicGraph();
+            for (String phrase : phrases) {
+                double conf = dbInterface.checkTokenInDatabase(phrase, basicGraphClass.getGraph())*0.8;   
+                conf += manager.nGram(phrase, 3)*0.2;
+                //System.out.println(phrase + "| "+ conf);
+                highlightPhrase(phrase, (int)conf);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     private Color getColorForNumber(int number) {
-        float hue = (float) number / 100; // Hue ranges from 0 to 1
-        return Color.getHSBColor(hue, 1, 1);
+        float hue = ((float) number / 100); // Hue ranges from 0 to 1
+        //System.out.println(hue);
+        return Color.getHSBColor(0, hue, 1);
     }
 
     private void highlightPhrase(String phrase, int colorInd) {
